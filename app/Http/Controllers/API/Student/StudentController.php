0741288dff95e28ad95813352bab7b613206492d
niwang09 <niwang101@gmail.com>
2021-05-14 15:33:57 +0800

    update

--- app/Http/Controllers/API/Student/StudentController.php
+++ app/Http/Controllers/API/Student/StudentController.php
@@ -103,36 +103,72 @@ class StudentController extends Controller
     public function show(Student $student){
         // $data = [];
         $person = $student->party->person;
-        $funded_detail = $student->funded_details()->select([
-                'unique_student_id',
-                'highest_school_level_completed_id',
-                'indigenous_status_id',
-                'location',
-                'language_id',
-                'labour_force_status_id',
-                'country_id',
-                'disability_flag',
-                'disability_ids',
-                'disability_ids',
-                'prior_educational_achievement_ids',
-                'at_school_flag',
-                'institute',
-                'statistical_area_level_1_id',
-                'statistical_area_level_2_id',
-                'aiss_check_date',
-                'year_completed',
-                'verified',
-                'verified_by',
-                'verified_date',
-                ])->get();
+        $funded_detail = $student->funded_detail()->select([
+                    'unique_student_id',
+                    'highest_school_level_completed_id',
+                    'indigenous_status_id',
+                    'location',
+                    'language_id',
+                    'labour_force_status_id',
+                    'country_id',
+                    'disability_flag',
+                    'disability_ids',
+                    'disability_ids',
+                    'prior_educational_achievement_ids',
+                    'at_school_flag',
+                    'institute',
+                    'statistical_area_level_1_id',
+                    'statistical_area_level_2_id',
+                    'aiss_check_date',
+                    'year_completed',
+                    'verified',
+                    'verified_by',
+                    'verified_date',
+                ])->first();
+        $contact_detail = $student->contact_detail()->select([
+                    'addr_suburb',
+                    'addr_postal_delivery_box',
+                    'addr_street_name',
+                    'addr_street_num',
+                    'addr_flat_unit_detail',
+                    'addr_building_property_name',
+                    'postcode',
+                    'state_id',
+                    'phone_home',
+                    'phone_work',
+                    'phone_mobile',
+                    'email',
+                    'email_at',
+                    "emer_name",
+                    "emer_address",
+                    "emer_telephone",
+                    "emer_relationship"
+                ])->first();
+        $visa = $student->visa_details()->select([
+                'nationality',
+                'passport_number',
+                'issue_date',
+                'expiry_date',
+                'visa_type',
+                'subclass',
+                'expiry_date_visa_type',
+                'applied_for_au_residency',
+                'study_rights'
+            ])->first();
+
+        $course = $student->funded_course;
+
         $data = [
-            'firstname' => $person->firstname,
-            'middlename' => $person->middlename,
-            'lastname' => $person->lastname,
-            'dob' => $person->date_of_birth,
-            'gender' => $person->gender,
-            'prefix' => $person->prefix,
-            'funded_detail' => $funded_detail
+            'firstname'     => $person->firstname,
+            'middlename'    => $person->middlename,
+            'lastname'      => $person->lastname,
+            'dob'           => $person->date_of_birth,
+            'gender'        => $person->gender,
+            'prefix'        => $person->prefix,
+            'funded_detail' => $funded_detail,
+            'contact'       => $contact_detail,
+            'visa'          => $visa,
+            'course'        => $course
         ];
         return $data;
     }
