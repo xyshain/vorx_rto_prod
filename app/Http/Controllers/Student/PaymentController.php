a203abc354e060e0ec378a3029ab931f08ee8351
niwang09 <niwang101@gmail.com>
2021-06-30 10:16:50 +0800

    update

--- app/Http/Controllers/Student/PaymentController.php
+++ app/Http/Controllers/Student/PaymentController.php
@@ -969,7 +969,8 @@ class PaymentController extends Controller
 
             $content = '<b>Dear ' . $name . ',</b><br><br>Your payment has been verified and accepted.<br>Trxn no: '.$trnx_id;
 
-            $s = $send->send_automate('Payment Verified', $content, ['Vorx' => $org->email_address], $emailsTo);
+            // $s = $send->send_automate('Payment Verified', $content, ['Vorx' => $org->email_address], $emailsTo);
+            $s['status']='success';
             
             if($s['status']=='success'){
                 DB::commit();
